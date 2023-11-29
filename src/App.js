38e15bda439734dcd715ceba42c0a502a18e3f42
Habeebope <habeebullahokunlola@gmail.com>
2023-11-29 11:08:44 +0100

    Added few design and made some changes to the application

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 import Home from "./Home";
-// import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className="double-section">
-        <Home />
-      </div>
+      <Home />
     </div>
   );
 }
 
 export default App;
-/*
-<Routes>
-<Route path="/" element={<Post />} />
-<Route path="/create" element={<Create />}/>
-</Routes>
- */
